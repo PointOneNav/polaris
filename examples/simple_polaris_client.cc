57ec9974d02d6b51cb7e286be962ecc68af044ac
Adam Shapiro <adam@pointonenav.com>
2021-03-14 21:18:13 -0400

    Refactored simple C++ client example for new API.

--- examples/simple_polaris_client.cc
+++ examples/simple_polaris_client.cc
@@ -1,13 +1,27 @@
-// Copyright (C) Point One Navigation - All Rights Reserved
+/**************************************************************************/ /**
+ * @brief Simple example of connecting to the Polaris service to receive
+ *        corrections.
+ *
+ * Copyright (c) Point One Navigation - All Rights Reserved
+ ******************************************************************************/
 
 #include <iomanip>
 #include <iostream>
-
-#include <boost/asio.hpp>
+#include <signal.h>
 
 #include <gflags/gflags.h>
+#include <glog/logging.h>
 
-#include <point_one/polaris_asio_client.h>
+#include <point_one/polaris/polarispp.h>
+
+// Allows for prebuilt versions of gflags/google that don't have gflags/google
+// namespace.
+namespace gflags {}
+namespace google {}
+using namespace gflags;
+using namespace google;
+
+using namespace point_one::polaris;
 
 // Polaris options:
 DEFINE_string(polaris_api_key, "",
@@ -17,51 +31,59 @@ DEFINE_string(polaris_api_key, "",
 DEFINE_string(polaris_unique_id, "device12345",
               "The unique ID to assign to this Polaris connection.");
 
-// Allows for prebuilt versions of gflags/google that don't have gflags/google
-// namespace.
-namespace gflags {}
-namespace google {}
-using namespace gflags;
-using namespace google;
+PolarisClient* polaris_client = nullptr;
 
 // Process receiver incoming messages.  This example code expects received data
 // to be ascii nmea messages.
-void ReceivedData(const void* data, size_t length) {
+void ReceivedData(const uint8_t* data, size_t length) {
   LOG(INFO) << "Received " << length << " bytes.";
+}
+
+void HandleSignal(int sig) {
+  signal(sig, SIG_DFL);
+
+  LOG(INFO) << "Caught signal " << sys_siglist[sig] << " (" << sig
+            << "). Closing Polaris connection.";
+  polaris_client->Disconnect();
 }
 
 int main(int argc, char* argv[]) {
   // Parse commandline flags.
+  FLAGS_logtostderr = true;
+  FLAGS_colorlogtostderr = true;
   ParseCommandLineFlags(&argc, &argv, true);
 
   // Setup logging interface.
   InitGoogleLogging(argv[0]);
 
-  // Setup asio work loop.
-  boost::asio::io_service io_loop;
-  boost::asio::io_service::work work(io_loop);
-
   // Construct a Polaris client.
   if (FLAGS_polaris_api_key == "") {
-    LOG(FATAL) << "You must supply a Polaris API key to connect to the server.";
+    LOG(ERROR) << "You must supply a Polaris API key to connect to the server.";
     return 1;
   }
 
-  auto connection_settings = point_one::polaris::DEFAULT_CONNECTION_SETTINGS;
-  point_one::polaris::PolarisAsioClient polaris_client(
-      io_loop, FLAGS_polaris_api_key, FLAGS_polaris_unique_id,
-      connection_settings);
-  polaris_client.SetPolarisBytesReceived(
+  polaris_client =
+      new PolarisClient(FLAGS_polaris_api_key, FLAGS_polaris_unique_id);
+  polaris_client->SetRTCMCallback(
       std::bind(&ReceivedData, std::placeholders::_1, std::placeholders::_2));
-  // Application can set position at any time to change associated beacon(s) and
-  // corrections. Example setting postion to SF in ECEF meters.
-  // It is required to call SetPositionECEF at some cadence to assure the
-  // corrections received are the best and relevant.
-  polaris_client.SetPositionECEF(-2707071, -4260565, 3885644);
-  polaris_client.Connect();
 
-  // Run work loop.
-  io_loop.run();
+  // Send the receiver's position to Polaris (this example is a position in San
+  // Francisco). Sending your position allows Polaris to associate you with an
+  // appropriate corrections stream. You must send a position at least once
+  // before any data will be sent back. You should periodically update your
+  // position as you move to make sure you are receiving the best set of
+  // corrections.
+  LOG(INFO) << "Setting initial position.";
+  polaris_client->SendECEFPosition(-2707071.0, -4260565.0, 3885644.0);
+
+  // Run the client, receiving data until the user presses Ctrl-C.
+  signal(SIGINT, HandleSignal);
+  signal(SIGTERM, HandleSignal);
+
+  LOG(INFO) << "Connecting to Polaris and listening for data...";
+  polaris_client->Run();
+
+  delete polaris_client;
 
   return 0;
 }
